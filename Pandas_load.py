dae7a246785b9c0c0a5503e6fab1fd6afeadb14e
Elena Fusillo <elena.fusillo@studio.unibo.it>
2022-12-06 18:00:07 +0100

    Folder independent data loading

--- Pandas_load.py
+++ Pandas_load.py
@@ -4,12 +4,18 @@ import numpy as np
 import glob
 import os
 
-#BUG: è computer-specific, trova un modo per renderlo fruibile da ovunque
-# Get CSV files list from a folder
-path = 'C:/Users/Elena/Documents/UNIVERSITA/2_Pattern Recognition/Project_exam/Air_quality'
+#NECESSARIO AVERE UNA CERTA "FOLDER ORGANIZATION"
+#Get CSV files list from a folder
+path = os.getcwd()
 all_files = glob.glob(path + "/CSV Stazione_Parametro_AnnoMese/*.csv")
 #all_files: lista di 2702 stringhe
 
+#LOAD THE DATA
+all_dfs = []
+for file in all_files:
+    my_data = pd.read_csv(file, header = 0)
+    all_dfs.append(my_data)
+#all_dfs: lista di 2702 elementi, ciascun elemento è un Pandas dataframe
 
 #LOAD THE LOOK UP TABLES
 params_lut = pd.read_excel(path+'/parametri.xlsx')
@@ -19,10 +25,3 @@ Cod_staz = stations_lut['Cod_staz'].unique()
 IdParametro = params_lut['IdParametro'].unique()
 #IdParametro: <class 'numpy.ndarray'> pieno di <class 'numpy.float64'>
 
-
-#LOAD THE DATA
-all_dfs = []
-for file in all_files:
-    my_data = pd.read_csv(file, header = 0)
-    all_dfs.append(my_data)
-#dati compresi dentro all_dfs: lista di 2702 elementi, ciascun elemento è un Pandas dataframe
